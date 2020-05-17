f1f7842dd2ed110e9958f56d79ec504ae5d2bcd6
Bryan Newbold <bnewbold@archive.org>
2020-05-16 18:40:26 -0700

    crude djvu XML parsing

---
+++ tests/test_djvu_parse.py
@@ -0,0 +1,16 @@
+
+from io import StringIO
+from fatcat_scholar.djvu import djvu_extract_leaf_texts
+
+
+def test_djvu_extract_leaf_texts():
+
+    # https://archive.org/details/ERIC_ED441501
+    with open('tests/files/ERIC_ED441501_djvu.xml', 'r') as f:
+        blob = f.read()
+
+    leaves = djvu_extract_leaf_texts(StringIO(blob), [3,6])
+    assert 3 in leaves
+    assert 6 in leaves
+    assert "2. Original cataloging tools" in leaves[3]
+    assert len(leaves) == 2
