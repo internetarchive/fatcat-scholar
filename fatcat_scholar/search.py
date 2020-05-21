01ae25c1bf24c8d9f7721f49122a15bf522bdbb4
Bryan Newbold <bnewbold@archive.org>
2020-05-21 14:23:31 -0700

    search query improvements
    - wire up most of the filters and sort order
    - query sticks around in search box
    - crude error message (needs work)

--- fatcat_scholar/search.py
+++ fatcat_scholar/search.py
@@ -3,40 +3,170 @@
 Helpers to make elasticsearch queries.
 """
 
+import sys
 import json
 import datetime
-
 import elasticsearch
-from elasticsearch_dsl import Search, Q
+from pydantic import BaseModel
 from dynaconf import settings
+from dataclasses import dataclass
+from elasticsearch_dsl import Search, Q
+from typing import List, Dict, Tuple, Optional, Any, Sequence
 
 
-def generic_search_execute(search, limit=25, offset=0, deep_page_limit=2000):
+class FulltextQuery(BaseModel):
+    q: Optional[str] = None
+    limit: Optional[int] = None
+    offset: Optional[int] = None
+    filter_time: Optional[str] = None
+    filter_type: Optional[str] = None
+    filter_availability: Optional[str] = None
+    sort_order: Optional[str] = None
+    time_options: Any = {
+        "label": "Release Date",
+        "slug": "filter_time",
+        "default": "all_time",
+        "list": [
+            {"label": "All Time", "slug": "all_time"},
+            {"label": "Past Week", "slug": "past_week"},
+            {"label": "Past Year", "slug": "past_year"},
+            {"label": "Since 2000", "slug": "since_2000"},
+            {"label": "Before 1925", "slug": "before_1925"},
+        ],
+    }
+    type_options: Any = {
+        "label": "Resource Type",
+        "slug": "filter_type",
+        "default": "papers",
+        "list": [
+            {"label": "Papers", "slug": "papers"},
+            {"label": "Reports", "slug": "reports"},
+            {"label": "Datasets", "slug": "datasets"},
+            {"label": "Everything", "slug": "everything"},
+        ],
+    }
+    availability_options: Any = {
+        "label": "Availability",
+        "slug": "filter_availability",
+        "default": "everything",
+        "list": [
+            {"label": "Everything", "slug": "everything"},
+            {"label": "Fulltext", "slug": "fulltext"},
+            {"label": "Open Access", "slug": "oa"},
+        ],
+    }
+    sort_options: Any = {
+        "label": "Sort Order",
+        "slug": "sort_order",
+        "default": "relevancy",
+        "list": [
+            {"label": "All Time", "slug": "relevancy"},
+            {"label": "Recent First", "slug": "time_desc"},
+            {"label": "Oldest First", "slug": "time_asc"},
+        ],
+    }
+
+class FulltextHits(BaseModel):
+    count_returned: int
+    count_found: int
+    offset: int
+    limit: int
+    deep_page_limit: int
+    query_time_ms: int 
+    results: List[Any]
+
+
+def do_fulltext_search(query: FulltextQuery, deep_page_limit: int = 2000) -> FulltextHits:
+
+    es_client = elasticsearch.Elasticsearch(settings.ELASTICSEARCH_BACKEND)
+    search = Search(using=es_client, index=settings.ELASTICSEARCH_FULLTEXT_INDEX)
+
+    # Convert raw DOIs to DOI queries
+    if len(query.q.split()) == 1 and query.q.startswith("10.") and query.q.count("/") >= 1:
+        search = search.filter("terms", doi=query.q)
+        query.q = "*"
+
+    # type filters
+    if query.filter_type == "papers":
+        search = search.filter("terms", type=[ "article-journal", "paper-conference", "chapter", ])
+    elif query.filter_type == "reports":
+        search = search.filter("terms", type=[ "report", "standard", ])
+    elif query.filter_type == "datasets":
+        search = search.filter("terms", type=[ "dataset", "software", ])
+    elif query.filter_type == "everything" or query.filter_type == None:
+        pass
+    else:
+        raise ValueError(f"Unknown 'filter_type' parameter value: '{query.filter_type}'")
+
+    # time filters
+    if query.filter_time == "past_week":
+        week_ago_date = str(datetime.date.today() - datetime.timedelta(days=7))
+        search = search.filter("range", date=dict(gte=week_ago_date))
+    elif query.filter_time == "past_year":
+        # (date in the past year) or (year is this year)
+        # the later to catch papers which don't have release_date defined
+        year_ago_date = str(datetime.date.today() - datetime.timedelta(days=365))
+        this_year = datetime.date.today().year
+        search = search.filter(Q("range", date=dict(gte=year_ago_date)) | Q("term", year=this_year))
+    elif query.filter_time == "since_2000":
+        search = search.filter("range", year=dict(gte=2000))
+    elif query.filter_time == "before_1925":
+        search = search.filter("range", year=dict(lt=1925))
+    elif query.filter_time == "all_time" or query.filter_time == None:
+        pass
+    else:
+        raise ValueError(f"Unknown 'filter_time' parameter value: '{query.filter_time}'")
+
+    search = search.query(
+        'query_string',
+        query=query.q,
+        default_operator="AND",
+        analyze_wildcard=True,
+        lenient=True,
+        fields=[
+            "everything",
+            "abstracts_all",
+            "fulltext.body",
+            "fulltext.annex",
+        ],
+    )
+    search = search.highlight(
+        "abstracts_all",
+        "fulltext.body",
+        "fulltext.annex",
+        number_of_fragments=2,
+        fragment_size=250,
+    )
+
+    # sort order
+    if query.sort_order == "time_asc":
+        search = search.sort("year", "date")
+    elif query.sort_order == "time_desc":
+        search = search.sort("-year", "-date")
+    elif query.sort_order == "relevancy" or query.sort_order == None:
+        pass
+    else:
+        raise ValueError(f"Unknown 'sort_order' parameter value: '{query.sort_order}'")
 
     # Sanity checks
-    if limit > 100:
-        limit = 100
-    if offset < 0:
-        offset = 0
+    limit = min((int(query.limit or 25), 100))
+    offset = max((int(query.offset or 0), 0))
     if offset > deep_page_limit:
         # Avoid deep paging problem.
         offset = deep_page_limit
 
-    search = search[int(offset):int(offset)+int(limit)]
+    search = search[offset:offset+limit]
 
     try:
         resp = search.execute()
     except elasticsearch.exceptions.RequestError as e:
         # this is a "user" error
-        print("elasticsearch 400: " + str(e.info))
-        #flash("Search query failed to parse; you might need to use quotes.<p><code>{}: {}</code>".format(e.error, e.info['error']['root_cause'][0]['reason']))
-        # XXX: abort(e.status_code)
-        raise e
+        print("elasticsearch 400: " + str(e.info), file=sys.stderr)
+        raise ValueError(str(e.info))
     except elasticsearch.exceptions.TransportError as e:
         # all other errors
-        print("elasticsearch non-200 status code: {}".format(e.info))
-        # XXX: abort(e.status_code)
-        raise e
+        print("elasticsearch non-200 status code: {}".format(e.info), file=sys.stderr)
+        raise IOError(str(e.info))
 
     # convert from objects to python dicts
     results = []
@@ -58,76 +188,12 @@ def generic_search_execute(search, limit=25, offset=0, deep_page_limit=2000):
             if type(h[key]) is str:
                 h[key] = h[key].encode('utf8', 'ignore').decode('utf8')
 
-    return {
-        "count_returned": len(results),
-        "count_found": int(resp.hits.total),
-        "results": results,
-        "offset": offset,
-        "limit": limit,
-        "deep_page_limit": deep_page_limit,
-        "query_time_ms": int(resp.took),
-    }
-
-def do_fulltext_search(q, limit=25, offset=0, filter_time=None, filter_type=None):
-
-    # Convert raw DOIs to DOI queries
-    if len(q.split()) == 1 and q.startswith("10.") and q.count("/") >= 1:
-        q = 'doi:"{}"'.format(q)
-
-    es_client = elasticsearch.Elasticsearch(settings.ELASTICSEARCH_BACKEND)
-    search = Search(using=es_client, index=settings.ELASTICSEARCH_FULLTEXT_INDEX)
-
-    # type filters
-    if filter_type == "papers":
-        search = search.filter("terms", type=[ "article-journal", "paper-conference", "chapter", ])
-    elif filter_type == "reports":
-        search = search.filter("terms", type=[ "report", "standard", ])
-    elif filter_type == "datasets":
-        search = search.filter("terms", type=[ "dataset", "software", ])
-    elif filter_type == "everything" or filter_type == None:
-        pass
-    else:
-        # XXX: abort(400)
-        raise Exception()
-
-    # time filters
-    if filter_time == "past_week":
-        week_ago_date = str(datetime.date.today() - datetime.timedelta(days=7))
-        search = search.filter("range", date=dict(gte=week_ago_date))
-    elif filter_time == "this_year":
-        search = search.filter("term", year=datetime.date.today().year)
-    elif filter_time == "since_2000":
-        search = search.filter("range", year=dict(gte=2000))
-    elif filter_time == "before_1925":
-        search = search.filter("range", year=dict(lt=1925))
-    elif filter_time == "all_time" or filter_time == None:
-        pass
-    else:
-        # XXX: abort(400)
-        raise Exception()
-
-    search = search.query(
-        'query_string',
-        query=q,
-        default_operator="AND",
-        analyze_wildcard=True,
-        lenient=True,
-        fields=[
-            "everything",
-            "abstracts_all",
-            "fulltext.body",
-            "fulltext.annex",
-        ],
+    return FulltextHits(
+        count_returned=len(results),
+        count_found=int(resp.hits.total),
+        offset=offset,
+        limit=limit,
+        deep_page_limit=deep_page_limit,
+        query_time_ms=int(resp.took),
+        results=results,
     )
-    search = search.highlight(
-        "abstracts_all",
-        "fulltext.body",
-        "fulltext.annex",
-        number_of_fragments=2,
-        fragment_size=250,
-    )
-
-    resp = generic_search_execute(search, offset=offset)
-
-    resp["query"] = { "q": q }
-    return resp
--- fatcat_scholar/web.py
+++ fatcat_scholar/web.py
@@ -4,25 +4,23 @@ This contains the FastAPI web application and RESTful API.
 So far there are few endpoints, so we just put them all here!
 """
 
+import sys
 from enum import Enum
-
 import babel.support
 from fastapi import FastAPI, APIRouter, Request, Depends, Header
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import HTMLResponse
-from pydantic import BaseModel
 from dynaconf import settings
+from typing import List, Dict, Tuple, Optional, Any, Sequence
 
 from fatcat_scholar.hacks import Jinja2Templates
-from fatcat_scholar.search import do_fulltext_search
+from fatcat_scholar.search import do_fulltext_search, FulltextQuery, FulltextHits
 
-print(settings.as_dict())
+print(f"dynaconf settings: {settings.as_dict()}", file=sys.stderr)
 
 I18N_LANG_TRANSLATIONS = ["de", "zh"]
 I18N_LANG_OPTIONS = I18N_LANG_TRANSLATIONS + [settings.I18N_LANG_DEFAULT,]
 
-class SearchParams(BaseModel):
-    q: str = ""
 
 class LangPrefix:
     """
@@ -61,7 +59,7 @@ async def home():
     return {"endpoints": {"/": "this", "/search": "fulltext search"}}
 
 @api.get("/search", operation_id="get_search")
-async def search(query: SearchParams = Depends(SearchParams)):
+async def search(query: FulltextQuery = Depends(FulltextQuery)):
     return {"message": "search results would go here, I guess"}
 
 web = APIRouter()
@@ -115,24 +113,48 @@ def load_i18n_templates():
 
 i18n_templates = load_i18n_templates()
 
+
 @web.get("/", include_in_schema=False)
 async def web_home(request: Request, lang: LangPrefix = Depends(LangPrefix), content: ContentNegotiation = Depends(ContentNegotiation)):
     if content.mimetype == "application/json":
         return await home()
     return i18n_templates[lang.code].TemplateResponse("home.html", {"request": request, "locale": lang.code, "lang_prefix": lang.prefix})
 
+
 @web.get("/about", include_in_schema=False)
 async def web_about(request: Request, lang: LangPrefix = Depends(LangPrefix)):
     return i18n_templates[lang.code].TemplateResponse("about.html", {"request": request, "locale": lang.code, "lang_prefix": lang.prefix})
 
+
 @web.get("/search", include_in_schema=False)
-async def web_search(request: Request, query: SearchParams = Depends(SearchParams), lang: LangPrefix = Depends(LangPrefix), content: ContentNegotiation = Depends(ContentNegotiation)):
+async def web_search(request: Request, query: FulltextQuery = Depends(FulltextQuery), lang: LangPrefix = Depends(LangPrefix), content: ContentNegotiation = Depends(ContentNegotiation)):
+
     if content.mimetype == "application/json":
         return await search(query)
-    found = None
-    if query.q:
-        found = do_fulltext_search(query.q)
-    return i18n_templates[lang.code].TemplateResponse("search.html", {"request": request, "locale": lang.code, "lang_prefix": lang.prefix, "found": found})
+    hits : Optional[FulltextHits] = None
+    search_error: Optional[str] = None
+    status_code: int = 200
+    if query.q is not None:
+        try:
+            hits = do_fulltext_search(query)
+        except ValueError as e:
+            search_error = str(e)
+            status_code = 400
+        except IOError as e:
+            search_error = str(e)
+            status_code = 500
+    return i18n_templates[lang.code].TemplateResponse(
+        "search.html",
+        {
+            "request": request,
+            "locale": lang.code,
+            "lang_prefix": lang.prefix,
+            "hits": hits,
+            "search_error": search_error,
+            "query": query,
+        },
+        status_code=status_code,
+    )
 
 
 app = FastAPI(
