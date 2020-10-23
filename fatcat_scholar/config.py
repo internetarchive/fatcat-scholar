65f880565d462950f4bcc10e77ad423d6cc3bf65
Bryan Newbold <bnewbold@archive.org>
2020-10-22 20:14:31 -0700

    make fmt

--- fatcat_scholar/config.py
+++ fatcat_scholar/config.py
@@ -3,4 +3,6 @@ from dynaconf import Dynaconf
 
 settings = Dynaconf(settings_file="settings.toml", environments=True,)
 
-GIT_REVISION = subprocess.check_output(["git", "describe", "--always"]).strip().decode('utf-8')
+GIT_REVISION = (
+    subprocess.check_output(["git", "describe", "--always"]).strip().decode("utf-8")
+)
