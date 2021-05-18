9c51894168032a40f2b595208db0ded0baee24bb
Bryan Newbold <bnewbold@archive.org>
2021-05-17 21:11:31 -0700

    sitemaps: remove date from sitmap URLs

--- extra/sitemap/README.md
+++ extra/sitemap/README.md
@@ -5,8 +5,7 @@ Requires [fatcat-cli](https://gitlab.com/bnewbold/fatcat-cli) and `jq`
 installed. Run these commands on a production machine.
 
     cd /srv/fatcat_scholar/sitemap
-    export DATE=`date --iso-8601`
-    /srv/fatcat_scholar/src/extra/sitemap/work_urls_query.sh $DATE
+    /srv/fatcat_scholar/src/extra/sitemap/work_urls_query.sh
     /srv/fatcat_scholar/src/extra/sitemap/generate_sitemap_indices.py
 
 ## Background
--- extra/sitemap/work_urls_query.sh
+++ extra/sitemap/work_urls_query.sh
@@ -4,10 +4,6 @@ set -e              # fail on error
 set -u              # fail if variable not set in substitution
 set -o pipefail     # fail if part of a '|' command fails
 
-: ${1?' You you did not supply a date argument'}
-
-# eg, 2020-08-19
-DATE="$1"
 
 # query for specific works; about 8.6 million circa 2021-04-29
 fatcat-cli search scholar 'doc_type:work (fulltext.access_type:ia_file OR fulltext.access_type:wayback) (NOT biblio.arxiv_id:*) (NOT biblio.pmcid:*) (NOT biblio.publisher_type:big5) (year:<1926 OR tags:*)' --index-json --limit 0 \
@@ -15,4 +11,4 @@ fatcat-cli search scholar 'doc_type:work (fulltext.access_type:ia_file OR fullte
     | jq .key -r \
     | tr '_' '/' \
     | awk '{print "https://scholar.archive.org/" $1}' \
-    | split --lines 20000 - sitemap-works-$DATE- -d -a 5 --additional-suffix .txt
+    | split --lines 20000 - sitemap-works- -d -a 5 --additional-suffix .txt
