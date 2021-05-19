42da448ea610afa980b5d893ce5982b98507fe3c
Bryan Newbold <bnewbold@archive.org>
2021-05-19 16:57:19 -0700

    sitemaps: filter out 12-digit wayback timestamp access URLs, for now

--- extra/sitemap/access_urls_query.sh
+++ extra/sitemap/access_urls_query.sh
@@ -7,9 +7,11 @@ set -o pipefail     # fail if part of a '|' command fails
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 # query for specific works; about 8.6 million circa 2021-04-29
+# NOTE: filter out 12-digit access URLs (vs. 14-digit)
 fatcat-cli search scholar 'doc_type:work (fulltext.access_type:ia_file OR fulltext.access_type:wayback) (NOT biblio.arxiv_id:*) (NOT biblio.pmcid:*) (NOT biblio.publisher_type:big5) (year:<1926 OR tags:*)' --index-json --limit 0 \
     | pv -l \
     | jq '[.fulltext.access_type, .fulltext.access_url] | @tsv' -r \
     | rg -v '^null' \
+    | rg -v 'web.archive.org/web/\d{12}/' \
     | $SCRIPT_DIR/transform_access_url.py \
     | split --lines 20000 - sitemap-access- -d -a 5 --additional-suffix .txt
--- extra/sitemap/work_urls_query.sh
+++ extra/sitemap/work_urls_query.sh
@@ -6,8 +6,10 @@ set -o pipefail     # fail if part of a '|' command fails
 
 
 # query for specific works; about 8.6 million circa 2021-04-29
+# NOTE: filter out 12-digit access URLs (vs. 14-digit)
 fatcat-cli search scholar 'doc_type:work (fulltext.access_type:ia_file OR fulltext.access_type:wayback) (NOT biblio.arxiv_id:*) (NOT biblio.pmcid:*) (NOT biblio.publisher_type:big5) (year:<1926 OR tags:*)' --index-json --limit 0 \
     | pv -l \
+    | rg -v 'web.archive.org/web/\d{12}/' \
     | jq .key -r \
     | tr '_' '/' \
     | awk '{print "https://scholar.archive.org/" $1}' \
