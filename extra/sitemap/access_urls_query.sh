8b4388eed8b0424d8d83b191a059037ee32d73b6
Bryan Newbold <bnewbold@archive.org>
2022-01-20 15:03:18 -0800

    sitemap generation: move public domain wall; remove 'is_biblio' filter

--- extra/sitemap/access_urls_query.sh
+++ extra/sitemap/access_urls_query.sh
@@ -8,8 +8,7 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 # query for specific works; about 8.6 million circa 2021-04-29
 # NOTE: filter out 12-digit access URLs (vs. 14-digit)
-fatcat-cli search scholar 'doc_type:work (fulltext.access_type:ia_file OR fulltext.access_type:wayback) (NOT biblio.arxiv_id:*) (NOT biblio.pmcid:*) ((NOT biblio.publisher_type:big5) OR year:<1926 OR tags:oa)' --index-json --limit 0 \
-    | jq -c 'select(.biblio.release_ident == .fulltext.release_ident)' \
+fatcat-cli search scholar 'doc_type:work (fulltext.access_type:ia_file OR fulltext.access_type:wayback) (NOT biblio.arxiv_id:*) (NOT biblio.pmcid:*) ((NOT biblio.publisher_type:big5) OR year:<=1926 OR tags:oa)' --index-json --limit 0 \
     | jq -r '[.work_ident, .fulltext.access_type, .fulltext.access_url] | @tsv' \
     | rg -v '^null' \
     | rg -v 'web.archive.org/web/\d{12}/' \
--- extra/sitemap/work_urls_query.sh
+++ extra/sitemap/work_urls_query.sh
@@ -7,8 +7,7 @@ set -o pipefail     # fail if part of a '|' command fails
 
 # query for specific works; about 8.6 million circa 2021-04-29
 # NOTE: filter out 12-digit access URLs (vs. 14-digit)
-fatcat-cli search scholar 'doc_type:work (fulltext.access_type:ia_file OR fulltext.access_type:wayback) (NOT biblio.arxiv_id:*) (NOT biblio.pmcid:*) ((NOT biblio.publisher_type:big5) OR year:<1926 OR tags:oa)' --index-json --limit 0 \
-    | jq -c 'select(.biblio.release_ident == .fulltext.release_ident)' \
+fatcat-cli search scholar 'doc_type:work (fulltext.access_type:ia_file OR fulltext.access_type:wayback) (NOT biblio.arxiv_id:*) (NOT biblio.pmcid:*) ((NOT biblio.publisher_type:big5) OR year:<=1926 OR tags:oa)' --index-json --limit 0 \
     | rg -v '^null' \
     | rg -v 'web.archive.org/web/\d{12}/' \
     | jq .key -r \
