8fa13a880a0f61966c46e76a83b7319a36f961d8
nate smith <nsmith@archive.org>
2024-05-21 16:11:05 -0700

    test for reference/match.json

--- tests/cat/refs.py
+++ tests/cat/refs.py
@@ -1,6 +1,5 @@
 # TODO references / reference matching
 
-# TODO /reference/match.json
 # TODO /release/{ident}/refs-out.json
 # TODO /release/{ident}/refs-in.json
 # TODO /release/{ident}/refs-out
@@ -11,16 +10,18 @@
 # TODO /wikipedia/{wiki_lang}:{wiki_article}/refs-out.json
 
 import datetime
+from urllib.parse import urlencode
 from fuzzycat.simple import FuzzyReleaseMatchResult, Status, Reason
 import scholar.cat.web
 
-# TODO /reference/match (POST, GET)
-def test_reference_match(client, fcclient, mocker, entities, es, es_resps):
+def test_reference_match(client, fcclient, mocker, entities):
     # on GET we render an empty form
     rv = client.get("/cat/reference/match")
     assert rv.status_code == 200
     assert "Matched Releases" not in rv.text
     assert "Parsed Citation" not in rv.text
+
+    # POST with submit_type=parse
 
     mocker.patch("scholar.cat.web.grobid_api_process_citation")
     scholar.cat.web.grobid_api_process_citation.return_value = '<biblStruct >\n\t<analytic>\n\t\t<title level="a" type="main">The winograd schema challenge</title>\n\t\t<author>\n\t\t\t<persName><forename type="first">Hector</forename><surname>Levesque</surname></persName>\n\t\t</author>\n\t\t<author>\n\t\t\t<persName><forename type="first">Ernest</forename><surname>Davis</surname></persName>\n\t\t</author>\n\t\t<author>\n\t\t\t<persName><forename type="first">Leora</forename><surname>Morgen- Stern</surname></persName>\n\t\t</author>\n\t</analytic>\n\t<monogr>\n\t\t<title level="m">Thirteenth International Conference on the Princi- ples of Knowledge Representation and Reasoning</title>\n\t\t\t\t<imprint>\n\t\t\t<date type="published" when="2012">2012</date>\n\t\t</imprint>\n\t</monogr>\n</biblStruct>\n'
@@ -53,6 +54,8 @@ def test_reference_match(client, fcclient, mocker, entities, es, es_resps):
     assert "steel and lace" in rv.text
     assert "no fulltext" in rv.text
 
+    # POST with submit_type=match
+
     mocker.patch("scholar.cat.web.close_fuzzy_biblio_matches")
     scholar.cat.web.close_fuzzy_biblio_matches.return_value = matches
 
@@ -74,3 +77,28 @@ def test_reference_match(client, fcclient, mocker, entities, es, es_resps):
     assert "UNKNOWN" in rv.text
     assert "steel and lace" in rv.text
     assert "no fulltext" in rv.text
+
+def test_reference_match_json(client, fcclient, mocker, entities):
+    matches = [FuzzyReleaseMatchResult(
+        status=Status.AMBIGUOUS, reason=Reason.UNKNOWN, release=entities["release"])]
+    mocker.patch("scholar.cat.web.close_fuzzy_biblio_matches")
+    scholar.cat.web.close_fuzzy_biblio_matches.return_value = matches
+    fcclient.get_release.return_value=entities["release"]
+
+    q = urlencode({"submit_type": "match",
+                  "raw_citation": "Hector+Levesque,+Ernest+Davis,+and+Leora+Morgen-+stern.+2012.+The+winograd+schema+challenge.+In+Thirteenth+International+Conference+on+the+Princi-+ples+of+Knowledge+Representation+and+Reasoning.",
+                  "title": "The+winograd+schema+challenge",
+                  "first_author": "Hector+Levesque",
+                  "journal": "Thirteenth+International+Conference+on+the+Princi-+ples+of+Knowledge+Representation+and+Reasoning",
+                  "year": "2012",
+                  "volume": "",
+                  "issue":"",
+                  "pages": ""})
+
+    rv = client.get("/cat/reference/match.json?"+q)
+    assert rv.status_code == 200
+    payload = rv.json()
+    assert len(payload) == 1
+    assert payload[0]["status"] == "ambiguous"
+    assert payload[0]["reason"] == "unknown"
+    assert payload[0]["release"]["title"] == "steel and lace"
